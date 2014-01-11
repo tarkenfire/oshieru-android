26accfe7537a3b08505ad6dc4412f0e0a42e7e65
Michael Mancuso <tarkenfire@fullsail.edu>
2014-01-11 06:54:42 -0500

    Stubbed UI Classes

--- src/com/hinodesoftworks/oshieru/MainMenuActivity.java
+++ src/com/hinodesoftworks/oshieru/MainMenuActivity.java
@@ -1,3 +1,10 @@
+/* 
+ * Date: Jan 11, 2014
+ * Project: Oshieru
+ * Package: com.hinodesoftworks.oshieru
+ * @author Michael Mancuso
+ *
+ */
 package com.hinodesoftworks.oshieru;
 
 import android.os.Bundle;
---
+++ src/com/hinodesoftworks/oshieru/VocabMasterListFragment.java
@@ -0,0 +1,13 @@
+/* 
+ * Date: Jan 11, 2014
+ * Project: Oshieru
+ * Package: com.hinodesoftworks.oshieru
+ * @author Michael Mancuso
+ *
+ */
+package com.hinodesoftworks.oshieru;
+
+public class VocabMasterListFragment
+{
+
+}
