38f4f70cf873bb23dc1c33189881aeaed7aee039
Michael Mancuso <tarkenfire@fullsail.edu>
2014-01-28 23:10:31 -0500

    End of Night Commit

--- src/com/hinodesoftworks/oshieru/CharacterQuizSetupFragment.java
+++ src/com/hinodesoftworks/oshieru/CharacterQuizSetupFragment.java
@@ -12,14 +12,33 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.NumberPicker;
 
 public class CharacterQuizSetupFragment extends Fragment
 {
+	NumberPicker questionNumPicker;
+	
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState)
 	{
 		return inflater.inflate(R.layout.fragment_quiz_setup, null);
 	}
+
+	@Override
+	public void onActivityCreated(Bundle savedInstanceState)
+	{
+		super.onActivityCreated(savedInstanceState);
+		
+		//there is no xml for setting these values, must be done in code.
+		questionNumPicker = (NumberPicker) getActivity().findViewById(R.id.quiz_setup_num_question_picker);
+		questionNumPicker.setMinValue(1);
+		questionNumPicker.setMaxValue(20);
+		questionNumPicker.setWrapSelectorWheel(false);
+		
+	}
+	
+	
+	
 	
 }
